98f1743fe82c0c6e82fc8a76caf4a11167318c4d
Zach Durham <zdurham101@gmail.com>
2020-12-14 22:17:48 -0500

    day one

---
+++ day_1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const NUM_TO_GET = 2020
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var numbers []int
+	for scanner.Scan() {
+		text := scanner.Text()
+		num, _ := strconv.Atoi(text)
+		numbers = append(numbers, num)
+	}
+outer:
+	for _, num := range numbers {
+		for _, innerNum := range numbers {
+			if innerNum != num {
+				total := innerNum + num
+
+				if total == NUM_TO_GET {
+					fmt.Printf("the answer is %d \n", num*innerNum)
+					break outer
+				}
+			}
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zdurham/aoc_2020
+
+go 1.13
